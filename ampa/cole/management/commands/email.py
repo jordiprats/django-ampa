de759f856ef6bc6ed9e9dd14326d8685f27d67cc
jordiprats <jordi.prats@gmail.com>
2020-10-14 23:16:34 +0200

    email prod

--- ampa/cole/management/commands/email.py
+++ ampa/cole/management/commands/email.py
@@ -26,13 +26,11 @@ class Command(BaseCommand):
                     html_message = '<html><body>Hola,<br>Des de l\'AMPA em demanen si podeu revisar les dades del vostre fill i donar el consentiment als delegats per fer-les servir per contactar amb vosaltres per temes del cole:<br><br><a href="http://ampa.systemadmin.es/alumnes/'+str(alumne.id)+'">http://ampa.systemadmin.es/alumnes/'+str(alumne.id)+'</a><br><br>Cal marcar la opció de cessió de dades per cada un dels pares, si voleu, rectificar si hi ha alguna dada incorrecte i al final hi ha també la opció per confirmar que les dades són correctes<br><br>Si us plau, no contesteu a aquest email, per qualsevol dubte contacteu amb el vostre delegat pels canals habituals<br><br>salutacions,</body></html>'
                     email_from = 'Delegats Lestonnac <no-reply@ampa.systemadmin.es>'
                     recipient_list = emails
-                    if 'jordi.prats@gmail.com' in emails:
-                        print('actual send!')
-                        send_mail( subject=subject, message=message, from_email=email_from, recipient_list=recipient_list, html_message=html_message)
-                        # print('subject:' +subject)
-                        # print('message:' +message)
-                        # print('html_message:' +html_message)
-                        # print('recipient_list:' +str(recipient_list))
+                    send_mail( subject=subject, message=message, from_email=email_from, recipient_list=recipient_list, html_message=html_message)
+                    # print('subject:' +subject)
+                    # print('message:' +message)
+                    # print('html_message:' +html_message)
+                    # print('recipient_list:' +str(recipient_list))
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
