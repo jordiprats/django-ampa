ecc9f000cb64774423ee7a12cdb8d505ae298b03
jordiprats <jordi.prats@gmail.com>
2020-10-24 13:02:45 +0200

    processingtool + PV k8s

---
+++ ampa/processingtool.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#             FREQUENCIES
+#            =============
+# import/export:    cada 5 segons aprox
+# email:            cada 5 minuts aprox
+
+while true
+do
+    for i in {1..30};
+    do
+        python /code/manage.py importxls
+        sleep 5
+        python /code/manage.py exportxls
+        sleep 5
+    done
+    python /code/manage.py email
+done
