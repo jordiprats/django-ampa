2c7e227e6721336faf271931c9eed81a19093f29
jordiprats <jordi.prats@gmail.com>
2021-02-20 19:50:16 +0100

    site admin nomes debug

--- ampa/ampa/urls.py
+++ ampa/ampa/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+import os
+
 urlpatterns = [
     path('prometheus/', include('django_prometheus.urls')),
     path('health/', include('health.urls')),
-    path('site-admin/', admin.site.urls),
     path('', include('cole.urls')),
     path('votacions/', include('voting.urls')),
     path('juntes/', include('peticions.urls'))
 ]
+
+if os.getenv('DEBUG', False):
+    urlpatterns.append(path('site-admin/', admin.site.urls))
