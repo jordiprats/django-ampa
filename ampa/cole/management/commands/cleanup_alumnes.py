45f8443c04780eb349bfa39ab219604426ad565a
Jordi Prats <jordi.prats@gmail.com>
2021-10-03 22:26:04 +0200

    cleanup

--- ampa/cole/management/commands/cleanup_alumnes.py
+++ ampa/cole/management/commands/cleanup_alumnes.py
@@ -8,12 +8,13 @@ class Command(BaseCommand):
   help = 'Cleanup Alumnes sense classes'
 
   def handle(self, *args, **options):
-    filename = options['file'][0]
-
+    
+    # Alumnes sense classes
     for alumne in Alumne.objects.filter(classes=None):
       print("Deleting alumne {}".format(alumne._get_print_name()))
       alumne.delete()
 
+    # fem save de tots
     for alumne in Alumne.objects.all():
       if alumne.classes.count() == 0:
         print("Deleting alumne {}".format(alumne._get_print_name()))
