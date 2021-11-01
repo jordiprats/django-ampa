edc055fcf63148d273332d37532d28da2b6b335c
Jordi Prats <jordi.prats@gmail.com>
2021-11-01 18:09:47 +0100

    probes

--- ampa/health/urls.py
+++ ampa/health/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('liveness', views.liveness, name='health.liveness'),
-    path('readiness', views.liveness, name='health.readiness'),
+    path('readiness', views.readiness, name='health.readiness'),
+    path('startup', views.startup, name='health.startup'),
 ]
--- ampa/health/views.py
+++ ampa/health/views.py
@@ -6,7 +6,15 @@ def liveness(request):
     return HttpResponse("OK")
 
 def readiness(request):
+    db_conn = connections['default']
+    try:
+        c = db_conn.cursor()
+    except OperationalError:
+        return HttpResponse("please check DB", status=500)
+    else:
+        return HttpResponse("OK")
 
+def startup(request):
     db_conn = connections['default']
     try:
         c = db_conn.cursor()
